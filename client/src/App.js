9459edba888eb1a31b83af5d6de8283e4f521f53
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-07-06 17:12:18 -0400

    other conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,8 @@ import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Main from './pages/Main'
 import Admin from './pages/Admin'
-<<<<<<< HEAD
 import AboutPage from './pages/About'
-=======
 import Err from './pages/404'
->>>>>>> master
 
 function App() {
   return (
@@ -18,13 +15,9 @@ function App() {
         <Nav />
           <Switch>
             <Route exact path="/" component={Main}/>
-<<<<<<< HEAD
             <Route exact path="/admin" component={Admin}/> 
             <Route exact path="/about" component={AboutPage}></Route>      
-=======
-            <Route exact path="/admin" component={Admin}/>
             <Route component={Err} />       
->>>>>>> master
           </Switch>
         <Footer />
       </div>
