af28d032a6f40a606859a48242b44009eae1a63c
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-07-09 18:57:24 -0400

    unit testing

---
+++ tests.js
@@ -0,0 +1,52 @@
+// const assert = require('assert')
+// // test command mocha
+
+// //Use a red line to indicate falures in testing
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   })
+
+// // Tests when travelMode is passed out of the getTravelMode function as a string value
+//     it(getTravelMode(a,b,c,d), () => {
+//     assert.equal(operations.validateString('travelMode'), true)
+//     })
+
+// //Test function to confirm both are numbers
+//     const validateNumbers = (originCoord, destinationCoord) => {
+//     if(isNaN(originCoord) || isNaN(destinationCoord)) {
+//         return false
+//         }
+//     return true
+//     }
+
+// //Tests that two numbers are used in Walk or Wait decision
+//     it(walkOrWait, () => {
+//     assert.equal(operations.validateNumbers(busData, walkData), true)
+//     })
+
+// //Tests walk or wait calculates the difference in times
+//     it(walkOrWait, () => {
+//     assert.equal(operations.subtract(busData, walkData),'')
+//   })
+
+// //Test that terminal when called will return true.
+//     it(terminal, () => {
+//     assert.equal(true, true)
+//     })
+
+// //Tests that bus bunch only returns boolean
+//     it(busBunch, () => {
+//     assert.equal(false, true)
+//     })
+
+// //Tests that stopId is always a number
+//     it(stopId, () => {
+//     assert.equal(operations.validateNumbers(), true)
+//     })
+
+// //Tests that noBusCondition always equals 999999 when used.
+//     it(noBusCondition, () => {
+//     assert.equal(999999)
+//     })
+    
