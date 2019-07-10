2fb28b1218dee83236e0ac41f922890f4ca28cf8
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-07-09 22:13:03 -0400

    route change

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.use("/admin", adminRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client","build", "index.html"));
 });
 
 module.exports = router;
